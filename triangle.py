3f444d873591c0353bbed1ba5d52f7d64ebb5c42
Palamarchuk Sofia <palamarchuksofia@yandex.ru>
2024-11-22 15:50:44 +0300

    Update triangle.py

--- triangle.py
+++ triangle.py
@@ -15,7 +15,7 @@ def area(a, b, c):
     raise ValueError("Входные данные должны быть неотрицательными числами.")
 
   if not (a + b > c and a + c > b and b + c > a):
-    raise ValueError("Указанные стороны не образуют действительный треугольник.")
+    raise ValueError("The provided sides do not form a valid triangle.")
 
   s = (a + b + c) / 2 # Полупериметр
   return math.sqrt(s * (s - a) * (s - b) * (s - c))
